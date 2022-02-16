da33a69775c0bd7810a3876dce46e12d04810879
Uri Shaked <uri@urishaked.com>
2022-02-16 17:48:06 +0200

    ci: update compile.sh for esp-idf-hal setup

--- support/esp-rs-rust/compile.sh
+++ support/esp-rs-rust/compile.sh
@@ -2,6 +2,7 @@
 
 cd rust-project
 (
-  cargo build --features "native" --release &&
-  python3 -m esptool --chip esp32 elf2image --flash_size 4MB target/xtensa-esp32-espidf/release/rust-project -o src/project.bin
-) > src/output_stdout.txt 2> src/output_stderr.txt
+  cat build/main.rs > examples/ledc-simple.rs &&
+  cargo +esp build --example ledc-simple --release --target xtensa-esp32-espidf &&
+  python3 -m esptool --chip esp32 elf2image --flash_size 4MB target/xtensa-esp32-espidf/release/examples/ledc-simple -o build/project.bin
+) > build/output_stdout.txt 2> build/output_stderr.txt
