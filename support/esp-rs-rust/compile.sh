de7632e042561edb95f8c0e75759bca0142dbb4d
Uri Shaked <uri@urishaked.com>
2022-02-26 17:55:30 +0200

    ci: update compile.sh to latest wokwi interface

--- support/esp-rs-rust/compile.sh
+++ support/esp-rs-rust/compile.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 cd rust-project
-(
-  cat build/main.rs > examples/ledc-simple.rs &&
-  cargo +esp build --example ledc-simple --release --target xtensa-esp32-espidf &&
-  python3 -m esptool --chip esp32 elf2image --flash_size 4MB target/xtensa-esp32-espidf/release/examples/ledc-simple -o build/project.bin
-) > build/output_stdout.txt 2> build/output_stderr.txt
+cat build-in/main.rs > examples/ledc-simple.rs && \
+cargo +esp build --example ledc-simple --release --target xtensa-esp32-espidf && \
+python3 -m esptool --chip esp32 elf2image --flash_size 4MB target/xtensa-esp32-espidf/release/examples/ledc-simple -o build-out/project.bin
